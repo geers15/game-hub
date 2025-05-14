d2d6af4f79dd40a2189c97cda7258a33f8648d9d
geers15 <gersong178@gmail.com>
2025-05-13 19:32:18 -0700

    recommit

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,21 +1,19 @@
 import { Game } from "@/hooks/useGames";
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
-import { PlatformIconList } from "./PlatformIconList";
 
 interface Props {
   game: Game;
 }
 
-export const GameCard = ({ game }: Props) => {
+const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <Heading>{game.name}</Heading>
       </CardBody>
     </Card>
   );
 };
+
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,7 @@ export const GameGrid = () => {
       {error && <Text color="red.500">{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding={10}
+        padding="10px"
         spacing={10}
       >
         {games.map((game) => (
