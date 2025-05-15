6f944b93b565f4494127d4ad22dbb39b4910f9bd
geers15 <gersong178@gmail.com>
2025-05-14 19:21:33 -0700

    commit2

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,5 @@
 import useGames from "@/hooks/useGames";
-import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 
