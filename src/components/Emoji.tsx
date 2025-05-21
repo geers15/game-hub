a3f120358e71582bc8afe10b30932549e6385570
geers15 <gersong178@gmail.com>
2025-05-21 10:04:36 -0700

    emoji

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,21 @@
+import bullsEye from "../assets/bulls-eye.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
+import meh from "../assets/meh.webp";
+import { ImageProps,Image } from "@chakra-ui/react";
+
+interface Props {
+  rating: number;
+}
+export const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh" },
+    4: { src: thumbsUp, alt: "recommended" },
+    5: { src: bullsEye, alt: "exceptional" },
+  };
+  return 
+    <Image {...emojiMap[rating] boxSize="25px"} />
+
+};
+
