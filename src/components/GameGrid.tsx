4416a17a3bc4e200670e31743a8704552979eca1
geers15 <gersong178@gmail.com>
2025-06-03 20:45:37 -0700

    refractor game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,6 +13,8 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
     <>
       {error && <Text color="red.500">{error}</Text>}
