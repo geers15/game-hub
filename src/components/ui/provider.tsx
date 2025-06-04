31b7f5da8884bc97e408ade5859a86b31bdafbeb
geers15 <gersong178@gmail.com>
2025-06-04 00:05:29 -0700

    Name change

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,12 +1,7 @@
 import * as React from "react";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-
+import { ChakraProvider, theme } from "@chakra-ui/react";
 type ProvidersProps = { children: React.ReactNode };
 
 export function Providers({ children }: ProvidersProps) {
-  return (
-    <ChakraProvider value={defaultSystem}>
-      {children}
-    </ChakraProvider>
-  );
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
